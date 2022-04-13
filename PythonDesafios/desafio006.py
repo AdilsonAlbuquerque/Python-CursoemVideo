61e3255ed557a3663b61e3b31b0e68f6812b2019
Adilson Nascimento <adilson.na@gmail.com>
2022-04-12 21:40:38 -0300

    Dobro, Triplo e Raiz Quadrada

---
+++ PythonDesafios/desafio006.py
@@ -0,0 +1,6 @@
+n = int(input('Digite um número: '))
+# d = n * 2
+# t = n * 3
+# r = n ** 0.5
+# print('O dobro é {}, o triplo é {} e a raiz é {:.0f}'.format(d, t, r))
+print('O dobro de {} vale {}, o triplo vale {}, a raiz quadrada vale {:.2f}.'.format(n, (n*2), (n*3), (n **(1/2))))
