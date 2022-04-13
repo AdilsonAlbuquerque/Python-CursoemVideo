4b0f2c74dc556d37eaef964b07368cf0fe403f49
Adilson Nascimento <adilson.na@gmail.com>
2022-04-12 21:44:36 -0300

    Separando dígitos de um número

---
+++ PythonDesafios/desafio023.py
@@ -0,0 +1,10 @@
+num = '0834'
+print(num.split())
+print(num)
+print('unidade: ' + num[3])
+print('dezena: ' + num[2])
+print('centena: ' + num[1])
+print('milhar: ' + num[0])
+
+#errado
+#correção no ex023.py
