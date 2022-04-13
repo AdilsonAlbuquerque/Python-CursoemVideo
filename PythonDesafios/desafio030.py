71680a21f24f9b452c79d99563c12823925c694b
Adilson Nascimento <adilson.na@gmail.com>
2022-04-12 22:13:25 -0300

    Par ou Ímpar

---
+++ PythonDesafios/desafio030.py
@@ -0,0 +1,5 @@
+n = int(input('Digite um número inteiro: '))
+if (n % 2) == 0:
+    print('Você digitou o número {} e ele é PAR!'.format(n))
+else:
+    print('Você digitou o número {} e ele é ÍMPAR!'.format(n))
