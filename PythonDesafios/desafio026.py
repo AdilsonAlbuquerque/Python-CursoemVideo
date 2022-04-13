4d3c6711f626add00814c1c73c20a12de4558b94
Adilson Nascimento <adilson.na@gmail.com>
2022-04-12 21:45:07 -0300

    Primeira e última ocorrência de uma string

---
+++ PythonDesafios/desafio026.py
@@ -0,0 +1,4 @@
+frase = input('Digite uma frase: ')
+print('A letra "A" aparece {} vezes'.format(frase.count('a')))
+print('A primeira vez na posição {}'.format(frase.find('a')))
+print('A última vez na posição {}'.format(frase.rfind('a')))
