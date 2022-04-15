1c537b0c342508f2b1d694ea76eb72198a4b7290
Adilson Nascimento <adilson.na@gmail.com>
2022-04-15 11:01:09 -0300

    update

--- PythonDesafios/desafio021.py
+++ PythonDesafios/desafio021.py
@@ -1,7 +1,7 @@
 import pygame
 pygame.mixer.init()
 pygame.init()
-pygame.mixer.music.load('classic_rock.mp3')
+pygame.mixer.music.load('desafio021.mp3')
 pygame.mixer.music.play()
 pygame.event.wait()
 
