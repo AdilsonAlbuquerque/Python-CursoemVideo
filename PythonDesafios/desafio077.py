33d443892d5f05d7c3b297d2f4aa24076feaf47c
Adilson Nascimento <adilson.na@gmail.com>
2022-04-17 20:25:34 -0300

    Contando vogais em Tupla

--- PythonDesafios/desafio077.py
+++ PythonDesafios/desafio077.py
@@ -2,9 +2,9 @@ palavras = ('aprender', 'programar', 'linguagem', 'python',
             'curso', 'gratis', 'estudar', 'praticar',
             'trabalhar', 'mercado', 'programador', 'futuro')
 
-for c in palavras:
-    print(f'\nNa palavra {c.upper()} temos', end=' ')
-    for vogal in c:
+for p in palavras:
+    print(f'\nNa palavra {p.upper()} temos', end=' ')
+    for vogal in p:
         if vogal in 'aeiou':
             print(vogal, end=' ')
 
